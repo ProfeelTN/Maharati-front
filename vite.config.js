0d839a53f3bd65c1848279c44aee937007bcafc4
maruukii <mohamedmagherbi9@gmail.com>
2024-09-20 06:49:53 +0100

    port modif

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,8 @@ export default defineConfig({
   define: {
     global: "window",
   },
+  server: {
+    host: "0.0.0.0", // Listen on all interfaces
+    port: process.env.PORT || 3000, // Use the port provided by Render or default to 3000
+  },
 });
